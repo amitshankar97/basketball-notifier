41341d25e2a37c1cc795e4973db1c045f94590ba
amitshankar97 <amitarunshankar@gmail.com>
2019-01-18 13:53:01 -0700

    logging

--- close_game.py
+++ close_game.py
@@ -1,74 +1,86 @@
-import requests
-import os
-from db import DB
+try:
+    import requests
+    import os
+    from db import DB
 
-CONFIG = {
-    'pt_differential': 10,
-    'mins_left': 6,
-    'period': 4
-}
-
-MAKER_URL = os.environ['MAKER_URL']
-NBA_BASE = 'https://data.nba.net/10s'
-INTERESTING_TEAMS = ['LAL', 'BOS', 'DAL', 'DEN', 'GSW', 'HOU', 'LAC', 'MEM', 'MIL', 'OKC', 'PHI', 'POR', 'TOR', 'UTA']
-
-def sendNotification(team1, team2, time):
-    payload = {
-        'value1': team1,
-        'value2': team2,
-        'value3': time
+    CONFIG = {
+        'pt_differential': 10,
+        'mins_left': 6,
+        'period': 4
     }
 
-    r = requests.post(MAKER_URL, data=payload)
+    MAKER_URL = os.environ['MAKER_URL']
+    NBA_BASE = 'https://data.nba.net/10s'
+    INTERESTING_TEAMS = ['LAL', 'BOS', 'DAL', 'DEN', 'GSW', 'HOU', 'LAC', 'MEM', 'MIL', 'OKC', 'PHI', 'POR', 'TOR', 'UTA']
 
-def NBATodayScoreboard():
-    r = requests.get(NBA_BASE + '/prod/v2/today.json').json()
-    todayScoreboard = r['links']['todayScoreboard']
-    return requests.get(NBA_BASE + todayScoreboard).json()
+    def sendNotification(team1, team2, time):
+        payload = {
+            'value1': team1,
+            'value2': team2,
+            'value3': time
+        }
 
-if __name__ == '__main__':
-    scoreboard = NBATodayScoreboard()
-    games = scoreboard['games']
+        r = requests.post(MAKER_URL, data=payload)
 
-    for game in games:
+    def NBATodayScoreboard():
+        r = requests.get(NBA_BASE + '/prod/v2/today.json').json()
+        todayScoreboard = r['links']['todayScoreboard']
+        return requests.get(NBA_BASE + todayScoreboard).json()
 
-        home = game['hTeam']
-        away = game['vTeam']
+    def logToMongo():
+        mongo = DB()
+        mongo.logToMongo()
 
-        if home['triCode'] in INTERESTING_TEAMS or away['triCode'] in INTERESTING_TEAMS:
-            if game['isGameActivated'] and game['period']['current'] >= CONFIG['period']:
+    if __name__ == '__main__':
+        scoreboard = NBATodayScoreboard()
+        games = scoreboard['games']
 
-                clock = game['clock'].split(':')
-                
-                if len(clock) < 2:
-                    minutes = 0
-                else:
-                    minutes = int(clock[0])
+        logToMongo()
 
-                pts_difference = abs(int(home['score']) - int(away['score']))
+        for game in games:
 
-                current_period = game['period']['current']
-                period = ""
-                if current_period > 4:
-                    period = (str(current_period % 4) + 'OT')
-                else:
-                    period = str(current_period) + 'Q'
+            home = game['hTeam']
+            away = game['vTeam']
 
-                if minutes <= CONFIG['mins_left'] and pts_difference <= CONFIG['pt_differential']:
-                    try:
-                        mongo = DB()
-                        db_game = mongo.find_game(game['gameId'])
+            if home['triCode'] in INTERESTING_TEAMS or away['triCode'] in INTERESTING_TEAMS:
+                if game['isGameActivated'] and game['period']['current'] >= CONFIG['period']:
 
-                        if db_game == None:
-                            time_left = period + ' ' + game['clock']
+                    clock = game['clock'].split(':')
+                    
+                    if len(clock) < 2:
+                        minutes = 0
+                    else:
+                        minutes = int(clock[0])
 
-                            home_text = home['triCode'] + ' ' + str(home['score'])
-                            away_text = away['triCode'] + ' ' + str(away['score'])
+                    pts_difference = abs(int(home['score']) - int(away['score']))
 
-                            sendNotification(home_text, away_text, time_left)
-                            mongo.insert_game(game['gameId'])
-                        else:
-                            continue
+                    current_period = game['period']['current']
+                    period = ""
+                    if current_period > 4:
+                        period = (str(current_period % 4) + 'OT')
+                    else:
+                        period = str(current_period) + 'Q'
 
-                    except Exception as e:
-                        print(e)
+                    if minutes <= CONFIG['mins_left'] and pts_difference <= CONFIG['pt_differential']:
+                        try:
+                            mongo = DB()
+                            db_game = mongo.find_game(game['gameId'])
+
+                            if db_game == None:
+                                time_left = period + ' ' + game['clock']
+
+                                home_text = home['triCode'] + ' ' + str(home['score'])
+                                away_text = away['triCode'] + ' ' + str(away['score'])
+
+                                sendNotification(home_text, away_text, time_left)
+                                mongo.insert_game(game['gameId'])
+                            else:
+                                continue
+
+                        except Exception as e:
+                            mongo = DB()
+                            mongo.logException(e)
+
+except Exception as e:
+    mongo = DB()
+    mongo.logException(e)
--- db.py
+++ db.py
@@ -1,15 +1,24 @@
 from pymongo import MongoClient
 import os
+from datetime import datetime
 
 client = MongoClient(os.environ['BASKETBALL_DB'])
 db = client['basketball']
 
 class DB:
     def __init__(self):
+        self.db = db
         self.games = db['close-game-tracker']
+        self.log = db['scrape-log']
 
     def find_game(self, gameId):
         return self.games.find_one({'gameId': gameId})
 
     def insert_game(self, gameId):
         return self.games.insert_one({'gameId': gameId, 'sentNotification': True})
+
+    def logToMongo(self):
+        self.log.insert_one({'time' : datetime.utcnow()})
+
+    def logException(self, e):
+        self.log.insert_one({'exception': e})
